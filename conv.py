0c83784f546dd161bf1e43546068952cc78b8615
Marion Lee <marion.bm.lee@gmail.com>
2024-11-28 14:20:03 +0100

    Ajout commentaires conv.py

--- conv.py
+++ conv.py
@@ -1,26 +1,25 @@
-# import openpyxl
 import pandas as pd
 import sqlite3
 
 # Noms des fichiers utilisés
-excel_file = "CatImpacts_Details.xlsx" 
-db_file = "ma_base.db"
+excel_file = "CatImpacts_Details.xlsx" #Fichier Excel
+db_file = "CatImpacts_sql.db"                  
 table_name = "CatImpacts_table" 
 
 # Chargement et transformation du dataframe
-cat_impacts = pd.read_excel(excel_file, sheet_name=0, index_col=1) # pandas détecte automatiquement les types de données 
-cat_impacts = cat_impacts.T
-cat_impacts.columns = cat_impacts.columns.str.strip()
-cat_impacts.set_index('UUID', inplace = True)
-cat_impacts.rename(columns={cat_impacts.columns[1]: "French Name" }, inplace = True)
-cat_impacts.drop(cat_impacts.index[0], inplace = True)
+cat_impacts = pd.read_excel(excel_file, sheet_name=0, index_col=1) # On crée un dataframe à partir de l'excel
+cat_impacts = cat_impacts.T                                          # On transpose pour avoir les caractéristiques en colonnes
+cat_impacts.columns = cat_impacts.columns.str.strip()               # On enlève les espaces inutiles dans les noms de colonnes
+cat_impacts.set_index('UUID', inplace = True)                          #La colonne UUID devient la colonne d'index 
+cat_impacts.rename(columns={cat_impacts.columns[1]: "French Name" }, inplace = True) # On nomme "French name" la colonne vide
+cat_impacts.drop(cat_impacts.index[0], inplace = True)                                  # On enlève les colonnes inutiles
 cat_impacts.drop(cat_impacts.columns[cat_impacts.nunique() == 1], axis=1, inplace=True)
-cat_impacts['Dataset format'] = 'ILCD format'
+cat_impacts['Dataset format'] = 'ILCD format'                               
 
-# Connexion à SGBD (ici SQLite)
+# Connexion à SGBD, ici SQLite
 conn = sqlite3.connect(db_file) 
 
-# Ecrit les infos stockées dans un DataFrame vers une base SQL
+# Ecrit les infos stockées dans le DataFrame vers une base SQL
 cat_impacts.to_sql(table_name, conn, if_exists='replace', index=False) # if_exists='replace' remplace la table si elle existe déjà, index=False évite d'ajouter un index 
 
 # Imprime une phrase après l'écriture dans la base SQL
